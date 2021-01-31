a19127f789c7cfca7d4948f5c45a113747b02237
alex10soi <54636750+alex10soi@users.noreply.github.com>
2021-01-31 18:08:42 +0200

    Add modal window on site

---
+++ ModalWindow JS/app.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('button');
+let modal = document.querySelector('.modal');
+let closeBtn = document.querySelector('.closeBtn');
+
+
+btn.addEventListener('click', function () {
+	modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', function () {
+	modal.style.display = 'none';
+});
+
+
+window.addEventListener('click', function (event) {
+	if(event.target == modal) {
+		modal.style.display = 'none';
+	}
+});
