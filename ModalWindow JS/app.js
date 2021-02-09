376231ea83f444913e514d10895ba9fc5041437b
Alex Saiko <alexustassoi@gmail.com>
2021-02-09 16:48:14 +0200

    Changed indent size of file

--- ModalWindow JS/app.js
+++ ModalWindow JS/app.js
@@ -3,17 +3,17 @@ let modal = document.querySelector('.modal');
 let closeBtn = document.querySelector('.closeBtn');
 
 
-btn.addEventListener('click', function () {
-	modal.style.display = 'flex';
+btn.addEventListener('click', function() {
+  modal.style.display = 'flex';
 });
 
-closeBtn.addEventListener('click', function () {
-	modal.style.display = 'none';
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
 });
 
 
-window.addEventListener('click', function (event) {
-	if(event.target == modal) {
-		modal.style.display = 'none';
-	}
+window.addEventListener('click', function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
 });
